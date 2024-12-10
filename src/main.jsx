050f572b7bb33ed806c781502b19412d39ba276d
dmisamohin <dmisamohin@team.amocrm.com>
2024-12-10 22:34:45 +0300

    deploy fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import '../src/styles/index.scss';
 import '../src/styles/fonts.scss';
 import '../src/styles/_mixins.scss';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <App />
+  // </StrictMode>,
+);
